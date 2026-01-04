f49365504090ef382ea3df2a111ef4502077d899
TerraFlopp <150452607+TerraFlopp@users.noreply.github.com>
2026-01-04 17:04:49 +0100

    Change logo image format from PNG to SVG

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ const Logo: React.FC<LogoProps> = ({ className = "w-12 h-12" }) => {
     <div className={`relative flex items-center justify-center ${className}`}>
       {/* On appelle ton fichier image situé dans le dossier public */}
       <img 
-        src="/logo-agrotech.png" 
+        src="/logo-agrotech.svg" 
         alt="AgroTech Logo" 
         className="w-full h-full object-contain"
       />
